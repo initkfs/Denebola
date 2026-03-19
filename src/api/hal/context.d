ba4aed4240f82436e0e323a72170069855d2a12b
initkfs <user@user>
2026-03-19 20:02:33 +0300

    Try to separate hal\com

--- src/api/hal/context.d
+++ src/api/hal/context.d
@@ -4,32 +4,27 @@
 module api.hal.context;
 
 import api.arch.riscv.versions;
+import api.hal.inits.hal_init;
 import ldc.attributes;
 
-__gshared extern (C)
+@halfunc __gshared
 {
-    void function(size_t* ctx) halSaveContext;
-    void function(size_t* ctx) halLoadContext;
-}
-
-__gshared void function() halSwitchInterruptContext;
-
-extern (C) void initialize()
-{
-    static if (__isRiscv)
+    extern (C)
     {
-        import ComContext = api.arch.riscv.boards.com.com_context;
-    }
-    else
-    {
-        static assert(false, "Not supported HAL context for platform");
+        void function(size_t* ctx) halSaveContext;
+        void function(size_t* ctx) halLoadContext;
     }
 
-    halSaveContext = &ComContext.comSaveContext;
-    halLoadContext = &ComContext.comLoadContext;
-    halSwitchInterruptContext = &ComContext.comSwitchInterruptContext;
-
-    assert(halSaveContext);
-    assert(halLoadContext);
-    assert(halSwitchInterruptContext);
+    void function() halSwitchInterruptContext;
 }
+
+static if (__isRiscv)
+{
+    import ComContext = api.arch.riscv.boards.com.com_context;
+}
+else
+{
+    static assert(false, "Not supported HAL context for platform");
+}
+
+mixin InitHalFuncs!ComContext;
--- src/api/hal/inits/hal_init.d
+++ src/api/hal/inits/hal_init.d
@@ -1,13 +1,15 @@
 module api.hal.inits.hal_init;
 
+import std.meta : AliasSeq;
+
+enum halfunc;
+
 /**
  * Authors: initkfs
  */
 
 void initialize()
 {
-    import std.meta : AliasSeq;
-
     import Context = api.hal.context;
 
     alias halModules = AliasSeq!(
@@ -17,5 +19,29 @@ void initialize()
     static foreach (m; halModules)
     {
         m.initialize;
+    }
+}
+
+mixin template InitHalFuncs(alias comModule, string comPrefix = "com", string halPrefix = "hal")
+{
+    extern (C) void initialize()
+    {
+        import std.traits : hasUDA;
+
+        alias currMod = mixin(__MODULE__);
+        static foreach (currMember; __traits(allMembers, currMod))
+        {
+            {
+                alias currHalFunc = __traits(getMember, currMod, currMember);
+                static if (hasUDA!(currHalFunc, halfunc))
+                {
+                    currHalFunc = &__traits(getMember, comModule, comPrefix ~ currMember[halPrefix
+                            .length .. $]);
+
+                    mixin("assert(", currMember, ",", "\"HAL function pointer '", currMember, "' must not be null in ", __traits(
+                            fullyQualifiedName, currMod), "\");");
+                }
+            }
+        }
     }
 }
