f404fc2cc9403a276909882051aafadee3a08108
initkfs <user@user>
2026-09-19 14:15:28 +0300

    Add c3 lp, debug assists

---
+++ src/api/arch/riscv/boards/esp32c3/c3_dbgassist.d
@@ -0,0 +1,65 @@
+module api.arch.riscv.boards.esp32c3.c3_dbgassist;
+
+import Bits = api.kstd.bits;
+import Volatile = api.arch.riscv.boards.com.com_volatile;
+
+/**
+ * Authors: initkfs
+ */
+
+enum ASSIST = 0x600C_E000;
+enum ASSIST_DEBUG_CORE_0_MONTR_ENA_REG = ASSIST;
+enum ASSIST_DEBUG_CORE_0_SP_SPILL_MIN_ENA_BIT = 8;
+enum ASSIST_DEBUG_CORE_0_SP_SPILL_MAX_ENA_BIT = 9;
+
+enum ASSIST_DEBUG_CORE_0_SP_MIN_REG = ASSIST + 0x0038;
+enum ASSIST_DEBUG_CORE_0_SP_MAX_REG = ASSIST + 0x003C;
+
+enum ASSIST_DEBUG_CORE_0_INTR_ENA_REG = ASSIST + 0x0008;
+enum ASSIST_DEBUG_CORE_0_SP_SPILL_MIN_INTR_ENA_BIT = 8;
+enum ASSIST_DEBUG_CORE_0_SP_SPILL_MAX_INTR_ENA_BIT = 9;
+
+enum ASSIST_DEBUG_CORE_0_INTR_RAW_REG = ASSIST + 0x0004;
+enum ASSIST_DEBUG_CORE_0_SP_SPILL_MIN_RAW_BIT = 8;
+enum ASSIST_DEBUG_CORE_0_SP_SPILL_MAX_RAW = 9;
+
+enum ASSIST_DEBUG_CORE_0_INTR_CLR_REG = ASSIST + 0x000C;
+enum ASSIST_DEBUG_CORE_0_SP_SPILL_MIN_CLR_BIT = 8;
+enum ASSIST_DEBUG_CORE_0_SP_SPILL_MAX_CLR_BIT = 9;
+
+bool isAssistSPIntr()
+{
+    auto reg = cast(size_t*) ASSIST_DEBUG_CORE_0_INTR_RAW_REG;
+    auto conf = Volatile.load(reg);
+    return Bits.bitIsSet(conf, ASSIST_DEBUG_CORE_0_SP_SPILL_MIN_RAW_BIT) || Bits.bitIsSet(conf, ASSIST_DEBUG_CORE_0_SP_SPILL_MAX_RAW);
+}
+
+void clearAssistIntr()
+{
+    auto reg = cast(size_t*) ASSIST_DEBUG_CORE_0_INTR_CLR_REG;
+    auto conf = Volatile.load(reg);
+    conf = Bits.bitClear(conf, ASSIST_DEBUG_CORE_0_SP_SPILL_MIN_CLR_BIT);
+    conf = Bits.bitClear(conf, ASSIST_DEBUG_CORE_0_SP_SPILL_MAX_CLR_BIT);
+    Volatile.save(reg, conf);
+}
+
+void assistEnableSP()
+{
+    auto reg = cast(size_t*) ASSIST_DEBUG_CORE_0_INTR_ENA_REG;
+    auto conf = Volatile.load(reg);
+    conf = Bits.bitSet(conf, ASSIST_DEBUG_CORE_0_SP_SPILL_MIN_INTR_ENA_BIT);
+    conf = Bits.bitSet(conf, ASSIST_DEBUG_CORE_0_SP_SPILL_MAX_INTR_ENA_BIT);
+    Volatile.save(reg, conf);
+
+    reg = cast(size_t*) ASSIST_DEBUG_CORE_0_MONTR_ENA_REG;
+    conf = Volatile.load(reg);
+    conf = Bits.bitSet(conf, ASSIST_DEBUG_CORE_0_SP_SPILL_MIN_ENA_BIT);
+    conf = Bits.bitSet(conf, ASSIST_DEBUG_CORE_0_SP_SPILL_MAX_ENA_BIT);
+    Volatile.save(reg, conf);
+}
+
+void assistSetSPRange(size_t minAddr, size_t maxAddr)
+{
+    Volatile.save(cast(size_t*) ASSIST_DEBUG_CORE_0_SP_MIN_REG, minAddr);
+    Volatile.save(cast(size_t*) ASSIST_DEBUG_CORE_0_SP_MAX_REG, maxAddr);
+}
---
+++ src/api/arch/riscv/boards/esp32c3/c3_lowpower.d
@@ -0,0 +1,25 @@
+module api.arch.riscv.boards.esp32c3.c3_lowpower;
+
+import Bits = api.kstd.bits;
+import Volatile = api.arch.riscv.boards.com.com_volatile;
+
+/**
+ * Authors: initkfs
+ */
+
+enum RTC = 0x6000_8000;
+
+enum RTC_CNTL_OPTION1_REG = RTC + 0x00F4;
+enum RTC_CNTL_FORCE_DOWNLOAD_BOOT_BIT = 0;
+
+size_t* calcRTC() => cast(size_t*) RTC;
+
+//EFUSE_DIS_FORCE_DOWNLOAD must be 0
+void switchToJointMode()
+{
+    auto reg = calcRTC;
+    auto conf = Volatile.load(reg);
+    conf = Bits.bitSet(conf, RTC_CNTL_FORCE_DOWNLOAD_BOOT_BIT);
+    Volatile.save(reg, conf);
+    //TODO reset cpu
+}
